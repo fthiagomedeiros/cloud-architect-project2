bcf58f0e6aba822f30335ba3298a3a39d45ce908
Francisco Thiago <fthiagomedeiros@gmail.com>
2020-07-05 23:12:43 -0300

    Terraform Exercise I

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,18 @@
 # TODO: Designate a cloud provider, region, and credentials
+provider "aws" {
+  access_key = $ACCESS_KEY
+  secret_key = "$SECRET_KEY"
+  region = "us-east-2"
+}
 
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
+resource "aws_instance" "ubuntu" {
+  count = "4"
+  ami = "ami-0a63f96e85105c6d3"
+  instance_type = "t2.micro"
 
-
-# TODO: provision 2 m4.large EC2 instances named Udacity M4
+  tags = {
+    Name = "Udacity T2"
+  }
+}
