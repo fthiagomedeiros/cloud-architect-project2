63610e1cc1ec77f830b6580dee1037d0a561a851
Francisco Thiago de Macedo Medeiros <francisco.medeiros@virtus.ufcg.edu.br>
2020-07-06 21:35:20 -0300

    Exercise 1 - 4

--- Task 5/Exercise_1/main.tf
+++ Task 5/Exercise_1/main.tf
@@ -7,12 +7,24 @@ provider "aws" {
 
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
-resource "aws_instance" "ubuntu" {
+resource "aws_instance" "ubuntu_t2" {
   count = "4"
   ami = "ami-0a63f96e85105c6d3"
   instance_type = "t2.micro"
 
   tags = {
     Name = "Udacity T2"
+  }
+}
+
+
+# TODO: provision 2 m4.large EC2 instances named Udacity M4
+resource "aws_instance" "ubuntu_m4" {
+  count = "2"
+  ami = "ami-0a63f96e85105c6d3"
+  instance_type = "m4.large"
+
+  tags = {
+    Name = "Udacity M4"
   }
 }
